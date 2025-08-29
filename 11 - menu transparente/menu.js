227aba14a35181eceec4f0b3df44b9a6ebb35737
WALLACE076300 <pedrosowallace630@gmail.com>
2025-08-29 00:00:48 -0300

    cansei...

--- 11 - menu transparente/menu.js
+++ 11 - menu transparente/menu.js
@@ -1,5 +1,19 @@
+// window.addEventListener("scroll", function(){
+//     let header = document.querySelector('#header')
+//     header.classList.toggle('rolagem', window.scrollY > 400)
+// })
 
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 500)
-})
+let lastScroll = 0;
+const menu = document.getElementById("menu");
+
+window.addEventListener("scroll", () => {
+    let currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll) {
+        menu.classList.add("hidden");
+    } else {
+        menu.classList.remove("hidden");
+    }
+
+    lastScroll = currentScroll;
+});
